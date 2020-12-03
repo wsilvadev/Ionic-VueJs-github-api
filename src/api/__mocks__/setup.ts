9f526dc30403afaf30ae49d0ca25db81ea04db6b
Willian <wsilvadev@gmail.com>
2020-12-03 08:43:03 -0300

    feat(api): create mocks to api setup

---
+++ src/api/__mocks__/setup.ts
@@ -0,0 +1,13 @@
+let api: any | undefined;
+
+function setup(): any {
+    if (!api) {
+        api = {
+            get: jest.fn(),
+            post: jest.fn()
+        };
+    }
+    return api;
+}
+
+export default setup;
